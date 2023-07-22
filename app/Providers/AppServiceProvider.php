e9d5fc175d253e503a878a2c96cba240f21aacaf
莊英祺 <a9872a9872@zhuangyingqideMacBook-Air.local>
2023-07-22 22:21:11 +0800

    build(telescope): add telescope package
    add telescope package and config the telescope setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
     }
 
     /**
